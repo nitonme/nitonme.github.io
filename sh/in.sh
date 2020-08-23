5a5d6662c77f040565ef9b4ca363bc88854ffa51
Lord J. Magnusson <54715115+nitonme@users.noreply.github.com>
2020-08-24 01:00:07 +0200

    Add files via upload

---
+++ sh/in.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Checking for root
+if [[ $EUID -ne 0 ]]; then
+   echo "Run as root."
+   exit 1
+fi
+
+# Installing the Connection Manager
+#curl -s https://packagecloud.io/install/repositories/asbru-cm/asbru-cm/script.deb.sh | sudo bash
+#sudo apt install asbru-cm
+
+echo "Hello World."
